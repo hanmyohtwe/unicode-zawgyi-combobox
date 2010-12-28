7282fb13862a57538d347bbdeef3d2ba36fab4ea
saturngod <saturngod@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-12-28 13:29:28 +0000

    first commit
    git-svn-id: https://plugins.svn.wordpress.org/unicode-zawgyi-combobox/trunk@326665 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ mm2zg.php
@@ -0,0 +1,24 @@
+<?php
+/*
+Plugin Name: Unicode & Zawgyi Combobox
+Plugin URI: http://www.saturngod.net
+Description: Combobox for convert between unicode and zawgyi. javascript is written by solvewaresolution. If you add this plugin, combobox will show and user can change zawgyi or unicode 5.1 to see your website or blog.
+Version: 0.1
+Author: Saturngod
+Author URI: http://en.saturngod.net
+*/
+
+if(!is_admin())
+{
+	add_action('wp_head', 'addscript');
+	
+}
+
+function addscript()
+{
+	$plugin_url= get_option('siteurl').'/wp-content/plugins/'.dirname(plugin_basename(__FILE__));
+	?>
+	<script src="<?php echo $plugin_url ?>/mm3zgcvtr.js"></script>
+<?php
+}
+?>
